319836a0fb2c3bf1e83bbdd1b182e1ac2f81356d
Rohit Gupta <rohitgupta74468@gmail.com>
2025-11-03 13:33:38 +0530

    Add main App component for Dockerized React app

---
+++ dockerize-react-multistage/app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Dockerized React App</h1>
+      <p>Served via Nginx (Multi-Stage Build)</p>
+    </div>
+  );
+}
+
+export default App;
